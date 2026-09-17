a6eff400fc0ea4dad60eb67aff5d5ac1f9ef623e
Peak Performance Gym Co <ewoodland2004@gmail.com>
2026-09-16 23:57:24 -0400

    Update cache version from v45 to v47

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='peak-performance-v45';
+const CACHE='peak-performance-v47';
 const STATIC_ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{

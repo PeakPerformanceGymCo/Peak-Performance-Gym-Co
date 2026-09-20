08d141a54e3f780c3f0a731e43b109bbb0e97528
Peak Performance Gym Co <ewoodland2004@gmail.com>
2026-09-20 14:14:29 -0400

    Update cache version to 'peak-performance-v55'

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='peak-performance-v54';
+const CACHE='peak-performance-v55';
 const STATIC_ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
